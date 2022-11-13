728c1745d52647fe5fe74448ac712f92e3b76612
Sapir <sapirlevi979@gmail.com>
2022-11-13 21:30:51 +0200

    fdgdfg

---
+++ less16_13_11_22/script.js
@@ -0,0 +1,12 @@
+//task 1
+
+//let copy = document.addEventListener("copy", function(){})
+
+function co (a) {
+    document.getElementById("h5").textContent= "you coppied: "+ document.getSelection();
+}
+
+
+//task 2
+document.addEventListener("keypress", function(a){document.getElementById("h1").textContent = "you clicked: "+`${a.code}`});
+
